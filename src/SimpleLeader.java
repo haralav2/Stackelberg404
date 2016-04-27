43ec2864fd3a45fa03efa3a3559afcb5d8370722
Veneta Haralampieva <veneta.haralampieva@student.manchester.ac.uk>
2016-04-27 11:48:17 +0100

    its not oops cause its correct - Veni 2016

--- src/SimpleLeader.java
+++ src/SimpleLeader.java
@@ -1,9 +1,11 @@
 import comp34120.ex2.PlayerImpl;
 import comp34120.ex2.PlayerType;
+import comp34120.ex2.ReactionFunction;
 import comp34120.ex2.Record;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -40,32 +42,61 @@ final class SimpleLeader
 	public void proceedNewDay(int p_date)
 		throws RemoteException
 	{
-		m_platformStub.publishPrice(m_type, genPrice(1.8f, 0.05f));
+		Record[] records = getPreviousRecords(m_type);
 
-//			m_platformStub.log(m_type, "" + m_platformStub.query(m_type,p_date-1));
-			m_platformStub.log(m_type, "" +calculateProfitOneDay(m_type,p_date-1));
+		m_platformStub.log(PlayerType.LEADER, "Records is: " + Arrays.toString(records));
 
-//		m_platformStub.log(PlayerType.LEADER, "Date 1:");
+		ReactionFunction followerReactionFunction = ReactionFunction.getFollowersReactionFunction(records);
+
+		m_platformStub.log(PlayerType.LEADER, "Reaction function is: " + followerReactionFunction);
+
+		double ourPrice = calculateBestStrategy(followerReactionFunction);
+
+		m_platformStub.log(PlayerType.LEADER, "Our price is: " + ourPrice);
+
+		m_platformStub.publishPrice(m_type, (float)ourPrice);
+
+		m_platformStub.log(m_type, "" +calculateProfitOneDay(m_type,p_date-1));
 	}
 
-	public double calculateProfitOneDay(PlayerType type, int day) throws RemoteException{
+
+	@Override
+	public void startSimulation(int p_steps)
+			throws RemoteException
+	{
+		Record[] records = getPreviousRecords(m_type);
+
+		double Pt = ReactionFunction.initializePt(records);
+		ReactionFunction theta = ReactionFunction.initializeTheta(records);
+
+		m_platformStub.log(m_type, "Initialized Pt = " + Pt);
+		m_platformStub.log(m_type, "Initialized theta = " + theta);
+	}
+
+	@Override
+	public void endSimulation()
+			throws RemoteException
+	{
+		m_platformStub.log(m_type, "Bye!");
+	}
+
+	private double calculateProfitOneDay(PlayerType type, int day) throws RemoteException{
 		Record record = m_platformStub.query(type,day);
 		return (record.m_leaderPrice - record.m_cost)*(2 - record.m_leaderPrice + 0.3*record.m_followerPrice);
-
 	}
 
+	private Record[] getPreviousRecords(PlayerType type) throws RemoteException{
+		Record[] records = new Record[100]; //todo
 
+		for(int i = 0; i < 100; i++){
+			records[i] = m_platformStub.query(type,i+1);
+		}
 
-	/**
-	 * Generate a random price based Gaussian distribution. The mean is p_mean,
-	 * and the diversity is p_diversity
-	 * @param p_mean The mean of the Gaussian distribution
-	 * @param p_diversity The diversity of the Gaussian distribution
-	 * @return The generated price
-	 */
-	private float genPrice(final float p_mean, final float p_diversity)
-	{
-		return (float) (p_mean + m_randomizer.nextGaussian() * p_diversity);
+		return records;
+	}
+
+	private static double calculateBestStrategy(ReactionFunction f){ //f is the opponent's reaction function
+		return ((0.3 * f.getbStar()) - (0.3 * f.getaStar()) - 3) / ((0.6 * f.getbStar()) - 2);
 	}
 
 	public static void main(final String[] p_args)
--- src/comp34120/ex2/ReactionFunction.java
+++ src/comp34120/ex2/ReactionFunction.java
@@ -1,5 +1,7 @@
 package comp34120.ex2;
 
+import java.rmi.RemoteException;
+
 /**
  * Created by mbax2vh2 on 22/04/16.
  */
@@ -9,6 +11,10 @@ public class ReactionFunction {
     public ReactionFunction(double aStar, double bStar) {
         this.aStar = aStar;
         this.bStar = bStar;
+    }
+
+    private double getFollowersApproxPrice(double leaderPrice){
+        return aStar + bStar * leaderPrice;
     }
 
     public double getbStar() {
@@ -36,6 +42,54 @@ public class ReactionFunction {
 
         return new ReactionFunction(aStar, bStar);
     }
+
+    private static final int HISTORICAL_DAYS = 100;
+    private static final double FORGETTING_FACTOR = 0.95;
+    private static double Pt = 0;
+    private static ReactionFunction theta = null;
+
+    public static double initializePt(Record[] records){
+        for(int d = 1; d <= HISTORICAL_DAYS; d++){ //TODO
+            double leaderPrice = records[d-1].m_leaderPrice;
+            Pt += Math.pow(FORGETTING_FACTOR, HISTORICAL_DAYS-d) * (1 + leaderPrice*leaderPrice);
+        }
+        return Pt;
+    }
+
+    public static ReactionFunction initializeTheta(Record[] records){
+        theta = getFollowersReactionFunction(records);
+        return theta;
+    }
+
+    /*
+    public static ReactionFunction recursiveLeastSquareApproach(Platform m_platformStub, PlayerType type, int day) throws RemoteException{
+        if(day == 1){
+            //base case
+        }
+        else {
+
+            ReactionFunction oldReactionFunction = recursiveLeastSquareApproach(m_platformStub, type, day-1);
+
+            Record record = m_platformStub.query(type,day);
+
+            double predictionError = record.m_followerPrice - oldReactionFunction.getFollowersApproxPrice(record.m_leaderPrice);
+            //todo floor
+
+        }
+
+    }
+
+    public static double calculateP(int day){
+        if(day == 100){
+
+        }
+        else{
+            double oldP = calculateP(day-1);
+
+
+        }
+    }
+`   */
 
     private static double getLeaderSum(Record[] records){
         double d = 0;
